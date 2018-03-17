ccabd6296f8ef67900c17576f427203a87b4d2bc
Lee Jones <leejones@leehawthorn.co.uk>
2018-03-17 06:45:20 +0000

    Wired uo Button to parent prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-
-
-
-const Button = () => {
-    const { buttonStyle } = styles;
+const Button = ({ onPress }) => {
+    const { buttonStyle, textStyle } = styles;
     
     return (
-        <TouchableOpacity style={buttonStyle}>
-            <Text>Click Me!</Text>
+        <TouchableOpacity onPress={onPress} style={buttonStyle}>
+            <Text style={textStyle}>
+                Click Me!
+            </Text>
         </TouchableOpacity>
     );
 };
 
 const styles = {
     textStyle: {
-
+        alignSelf: 'center',
+        color: '#007aff',
+        fontSize: 16,
+        fontWeight: '500',
+        paddingTop: 10,
+        paddingBottom: 10
     },
 
     buttonStyle: {
